55f47b69b89402564e3559c5eb3fe44861e2470c
shun <wezard706@gmail.com>
2021-02-11 20:20:27 +0900

    STest修正

--- src/main/java/com/sample/app/dao/FootballDataDao.java
+++ src/main/java/com/sample/app/dao/FootballDataDao.java
@@ -17,13 +17,12 @@ public class FootballDataDao {
 
   private static final String URL = "https://api.football-data.org/v2/players";
 
-  @Autowired
   public FootballDataDao(RestTemplate restTemplate) {
     this.restTemplate = restTemplate;
   }
 
   public ResponseEntity<Player> getPlayer(String id) {
-    UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.fromHttpUrl(URL).path(id);
+    UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.fromHttpUrl(URL).pathSegment(id);
     HttpHeaders httpHeaders = new HttpHeaders();
     httpHeaders.set("X-Auth-Token", "36350698d5bf4b41aca0094ddfddffe1");
     HttpEntity<?> httpEntity = new HttpEntity<>(httpHeaders);
