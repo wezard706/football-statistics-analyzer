f169f74bf9e5e0fc2718c7d80aefa0415741fdff
shun <wezard706@gmail.com>
2021-02-23 14:23:20 +0900

    matchFIlter追加

--- src/test/groovy/com/sample/app/dao/FootballDataDaoSTest.groovy
+++ src/test/groovy/com/sample/app/dao/FootballDataDaoSTest.groovy
@@ -1,20 +1,60 @@
 package com.sample.app.dao
 
 import com.sample.app.Application
+import com.sample.app.dao.client.FootballDataHttpClient
+import org.spockframework.spring.SpringBean
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer
 import org.springframework.test.context.ContextConfiguration
 import spock.lang.Specification
+import spock.lang.Unroll
 
 @ContextConfiguration(initializers = ConfigFileApplicationContextInitializer, classes = [Application])
 class FootballDataDaoSTest extends Specification {
 
   @Autowired
-  private FootballDataDao footballDataDao
+  private FootballDataDao target
+
+  @SpringBean
+  private FootballDataHttpClient footballDataHttpClient = Mock()
 
   def "getPlayer"() {
-    expect:
-    def result = footballDataDao.getPlayer("44")
-    println(result)
+    setup:
+    def playerId = "44"
+    1 * footballDataHttpClient.get(_ as String, _ as Class<Object>) >> {
+      def args1 = it.get(0)
+      args1 == "players/" + playerId
+      def args2 = it.get(1)
+      args2 == String.class
+      return
+    }
+
+    when:
+    target.getPlayer(playerId)
+
+    then:
+    notThrown(Exception)
+  }
+
+  @Unroll
+  def "getPlayer NG(#testCase)"() {
+    setup:
+    0 * footballDataHttpClient.get(_ as String, _ as Class<Object>)
+
+    when:
+    target.getPlayer(playerId)
+
+    then:
+    thrown(NullPointerException)
+
+    where:
+    testCase         | playerId
+    "playerId=empty" | ""
+    "playerId=null"  | null
+  }
+
+  @Unroll
+  def "getMatches"() {
+    // FIXME 実装する
   }
 }
---
+++ src/test/groovy/com/sample/app/dao/filter/MatchFilterSTest.groovy
@@ -0,0 +1,43 @@
+package com.sample.app.dao.filter
+
+import com.sample.app.Application
+import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer
+import org.springframework.test.context.ContextConfiguration
+import spock.lang.Specification
+import spock.lang.Unroll
+
+@ContextConfiguration(initializers = ConfigFileApplicationContextInitializer, classes = [Application])
+class MatchFilterSTest extends Specification {
+
+  @Unroll
+  def "getMatches(#testCase)"() {
+    when:
+    def result = matchFilter.toQueryString()
+
+    then:
+    result == expectation
+
+    where:
+    testCase     | matchFilter   | expectation
+    "0 filter"   | emptyFilter() | ""
+    "1 filter"   | oneFilter()   | "?dateFrom=2020-01-01"
+    "2 filter"   | twoFilter()   | "?dateFrom=2020-01-01&dateTo=2020-01-01"
+    "all filter" | allFilter()   | "?dateFrom=2020-01-01&dateTo=2020-01-01&stage=REGULAR_SEASON&status=FINISHED&matchday=21&group=Regular Season&season=2020"
+  }
+
+  def emptyFilter() {
+    return new MatchFilter.Builder().build()
+  }
+
+  def oneFilter() {
+    return new MatchFilter.Builder().dateFrom("2020-01-01").build()
+  }
+
+  def twoFilter() {
+    return new MatchFilter.Builder().dateFrom("2020-01-01").dateTo("2020-01-01").build()
+  }
+
+  def allFilter() {
+    return new MatchFilter.Builder().dateFrom("2020-01-01").dateTo("2020-01-01").stage("REGULAR_SEASON").status("FINISHED").matchDay("21").group("Regular Season").season("2020").build()
+  }
+}
