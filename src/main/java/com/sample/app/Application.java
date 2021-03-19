6230fc777f2c4073f7944ef1ce41ced6459f6c2f
shun <wezard706@gmail.com>
2021-03-19 21:59:16 +0900

    検証コード追加

--- src/main/java/com/sample/app/Application.java
+++ src/main/java/com/sample/app/Application.java
@@ -7,12 +7,19 @@ import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+import com.sample.app.service.FootballInfoSendService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @LineMessageHandler
 public class Application {
+
+  private final FootballInfoSendService footballInfoSendService;
+
+  Application(FootballInfoSendService footballInfoSendService) {
+    this.footballInfoSendService = footballInfoSendService;
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
@@ -21,6 +28,9 @@ public class Application {
   @EventMapping
   public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
     System.out.println("event: " + event);
+    final String userId = event.getSource().getUserId();
+    System.out.println("userId=" + userId);
+    footballInfoSendService.sendUpcomingMatches(userId);
     final String originalMessageText = event.getMessage().getText();
     return new TextMessage(originalMessageText);
   }
